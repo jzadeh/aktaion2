98aea7b3ebeca4662224594c9f79fc3525aa86b0
alexmzadeh <alexmzadeh+gh@gmail.com>
2017-09-11 17:41:49 -0700

    Added Host, URI features to generic proxy parser. Recast generic proxy parser epochTime type as pandas._libs.tslib.Timestamp datatype.

--- python/parserDev/GenericProxyParser.py
+++ python/parserDev/GenericProxyParser.py
@@ -15,7 +15,7 @@ def generic_line_parser(logLine):
     # example data
     #logLine = '[09/Jan/2014:04:53:04 -0800] "Nico Rosberg" 172.16.2.101 77.75.107.241 1500 200 TCP_HIT "GET http://www.divernet.com/ HTTP/1.1" "Internet Services" "low risk " "text/html; charset=utf-8" 470 396 "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)" "http://www.google.com/url?sa=t&rct=j&q=&esrc=s&frm=1&source=web&cd=1&sqi=2&ved=0CCoQFjAA&url=http%3A%2F%2Fwww.divernet.com%2F&ei=opvOUpyXFrSA2QXnv4DwDg&usg=AFQjCNHeSe4ebK0u69M-TBEGNkTZy-C-Nw&bvm=bv.59026428,d.b2I" "-" "0" "" "-" '
 
-    logPattern = '\[(?P<dateTime>.*?) (?P<timezone>.*?)\] \"(?P<userName>.*?)\" (?P<origHost>.*?) (?P<respHost>.*?) (?P<unknownValue>.*?) (?P<statusCode>.*?) (?P<cacheResult>.*?) \"(?P<httpMethod>.*?) (?P<fullUrl>.*?) HTTP/(?P<httpVersion>.*?)\" \"(?P<domainClassification>.*?)\" \"(?P<riskClassification>.*?)\" \"(?P<mimeType>.*?)\" (?P<bytesSent>.*?) (?P<bytesReceived>.*?) \"(?P<userAgentString>.*?)\" \"(?P<referrer>.*?)\" \"(?P<urlMeta1>.*?)\" \"(?P<urlMeta2>.*?)\" \"(?P<urlMeta3>.*?)\" \"(?P<urlMeta4>.*?)\"'
+    logPattern = '\[(?P<dateTime>.*?) (?P<timezone>.*?)\] \"(?P<userName>.*?)\" (?P<origHost>.*?) (?P<respHost>.*?) (?P<unknownValue>.*?) (?P<statusCode>.*?) (?P<cacheResult>.*?) \"(?P<httpMethod>.*?) (?P<fullUrl>.*?) HTTP/(?P<httpVersion>.*?)\" \"(?P<domainClassification>.*?)\" \"(?P<riskClassification>.*?)\" \"(?P<mimeType>.*?)\" (?P<bytesSent>.*?) (?P<bytesReceived>.*?) \"(?P<userAgent>.*?)\" \"(?P<referrer>.*?)\" \"(?P<urlMeta1>.*?)\" \"(?P<urlMeta2>.*?)\" \"(?P<urlMeta3>.*?)\" \"(?P<urlMeta4>.*?)\"'
 
     try:
         matched = re.match(logPattern, logLine)
@@ -49,6 +49,13 @@ def generic_line_parser(logLine):
 
     # Add uri key/value pair with urllib.parse
     matched_dict['uri'] = urllib.parse.urlparse(matched_dict['fullUrl']).path
+
+    # Add host key/value pair with urllib.parse
+    matched_dict['host'] = urllib.parse.urlparse(matched_dict['fullUrl']).netloc
+
+    # Cast epochTime into tslib.timeStamp datatype
+    matched_dict['epochTime'] = pd.to_datetime(matched_dict['epochTime'], unit='s')
+
 
     return(matched_dict)
 
@@ -100,7 +107,7 @@ def test_generic_line_parser():
  'urlMeta3': '',
  'urlMeta4': '-',
  'urlRequested': 'http://www.divernet.com/',
- 'userAgentString': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; '
+ 'userAgent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; '
                     'Trident/6.0)',
  'userName': 'Nico Rosberg',
  'referrer': 'http://www.google.com/url?sa=t&rct=j&q=&esrc=s&frm=1&source=web&cd=1&sqi=2&ved=0CCoQFjAA&url=http%3A%2F%2Fwww.divernet.com%2F&ei=opvOUpyXFrSA2QXnv4DwDg&usg=AFQjCNHeSe4ebK0u69M-TBEGNkTZy-C-Nw&bvm=bv.59026428,d.b2I'}
--- python/parserDev/scriptTest.py
+++ python/parserDev/scriptTest.py
@@ -5,6 +5,7 @@ import os
 import inspect
 from python.parserDev.broParse import broParse
 import urllib
+import pandas as pd
 
 
 # #directory = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
@@ -12,7 +13,7 @@ import urllib
 # #fileName = os.path.join(directory, '../../data/testData/badBroLog.log')
 #
 # #http.log
-#fileName = "/Users/Gary/PycharmProjects/Aktaion2/data/broData/ExploitExample/http.log"
+# fileName = "/Users/Gary/PycharmProjects/Aktaion2/data/broData/ExploitExample/http.log"
 #
 # #bro.csv
 # #fileName = "/Users/Gary/PycharmProjects/Aktaion2/data/testData/bro.csv"
@@ -39,9 +40,18 @@ import urllib
 # # #Popen("sudo ./p0f -i en0 -p", cwd="/Users/Gary/p0f-3.09b/")
 
 # #test GenericProxyParser.generic_proxy_parser
-df = gpp.generic_proxy_parser("/Users/Gary/PycharmProjects/Aktaion2/data/proxyData/exploitData/2014-01-02-neutrino-exploit-traffic.webgateway")
-pprint(df.head())
+proxy_df = gpp.generic_proxy_parser("/Users/Gary/PycharmProjects/Aktaion2/data/proxyData/exploitData/2014-01-02-neutrino-exploit-traffic.webgateway")
+#pprint(proxy_df)
 
+# #Test merge/normalization of bro and proxy logs
+fileName = "/Users/Gary/PycharmProjects/Aktaion2/data/broData/ExploitExample/http.log"
+bro_df = broParse.bro_http_to_df(fileName)
+#pprint(type(bro_df['epochTime'][0]))
+
+new_df = pd.concat([bro_df, proxy_df], axis=0)
+new_df = pd.DataFrame.reset_index(new_df)
+#pprint(type(new_df['epochTime'][4]))
+pprint(new_df['fullUrl'])
 # #test GenericProxyParser.generic_line_parser
 # logLine = '[09/Jan/2014:04:53:28 -0800] "Nico Rosberg" 172.16.2.101 85.93.134.203 1500 204 TCP_HIT "GET http://www.grad.ru/forum/public/js/3rd_party/colorpicker/0490c7979e/?f=s&k=5797540687062411 HTTP/1.1" "Internet Services" "low risk " "text/html" 329 203 "Mozilla/4.0 (Windows 7 6.1) Java/1.7.0_13" "" "-" "0" "" "-"'
 #
