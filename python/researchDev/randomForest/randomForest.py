f07a6b6b7338817b4a85089b23168813894c7a69
jzadeh <zadehjoseph@gmail.com>
2017-11-29 00:20:12 -0800

    Code cleanup, fix small bugs, update gitignore, rollback to working exploit behavior definitions.

--- python/researchDev/randomForest/randomForest.py
+++ python/researchDev/randomForest/randomForest.py
@@ -22,53 +22,54 @@ def make_training_class(df):
     df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
     return(df)
 
-# # Specify bro data directories
-# bro_exploit_dir = "/Users/Gary/PycharmProjects/Aktaion2/python/researchDev/randomForest/brologs/exploits"
-# bro_benign_dir = "/Users/Gary/PycharmProjects/Aktaion2/python/researchDev/randomForest/brologs/benign"
-#
-# # Declare global exploit and benign dataFrame vars
-# df_ex = pd.DataFrame()
-# df_be = pd.DataFrame()
-#
-# # Load exploit Bro data into df_ex
-# for filename in os.listdir(bro_exploit_dir):
-#     bro_df = br.bro_http_to_df(bro_exploit_dir + "/" + filename)
-#     df_ex = df_ex.append(bro_df)
-#
-# # Reset the df_ex rows index
-# df_ex = df_ex.reset_index()
-# # Add and populate threat_class column
-# df_ex['threat_class'] = 1
-#
-#
-# # Load benign Bro data into df_be
-# df_be = pd.DataFrame()
-# for filename in os.listdir(bro_benign_dir):
-#     bro_df = br.bro_http_to_df(bro_benign_dir + "/" + filename)
-#     df_be = df_be.append(bro_df)
-#
-# # Reset the df_be rows index
-# df_be = df_be.reset_index()
-# # Add and populate threat_class column
-# df_be['threat_class'] = 0
-#
-# # Add is_train column
-# df_ex = make_training_class(df_ex)
-# df_be = make_training_class(df_be)
-#
-# # Create two for each set new dataframes, one with the training rows, one with the test rows
-# train_ex, test_ex = df_ex[df_ex['is_train']==True], df_ex[df_ex['is_train']==False]
-# train_be, test_be = df_be[df_be['is_train']==True], df_be[df_be['is_train']==False]
-#
-# # Concatenate the test and training data into two seperate dataframes
-# train = pd.concat([train_ex, train_be])
-# test = pd.concat([test_ex, test_be])
-#
-#
-#
+
+# Specify bro data directories
+bro_exploit_dir = "data/brologs/exploits"
+bro_benign_dir = "data/brologs/benign"
+
+# Declare global exploit and benign dataFrame vars
+df_ex = pd.DataFrame()
+df_be = pd.DataFrame()
+
+# Load exploit Bro data into df_ex
+for filename in os.listdir(bro_exploit_dir):
+    bro_df = br.bro_http_to_df(bro_exploit_dir + "/" + filename)
+    df_ex = df_ex.append(bro_df)
+
+# Reset the df_ex rows index
+df_ex = df_ex.reset_index()
+# Add and populate threat_class column
+df_ex['threat_class'] = 1
+
+
+# Load benign Bro data into df_be
+df_be = pd.DataFrame()
+for filename in os.listdir(bro_benign_dir):
+    bro_df = br.bro_http_to_df(bro_benign_dir + "/" + filename)
+    df_be = df_be.append(bro_df)
+
+# Reset the df_be rows index
+df_be = df_be.reset_index()
+# Add and populate threat_class column
+df_be['threat_class'] = 0
+
+# Add is_train column
+df_ex = make_training_class(df_ex)
+df_be = make_training_class(df_be)
+
+# Create two for each set new dataframes, one with the training rows, one with the test rows
+train_ex, test_ex = df_ex[df_ex['is_train']==True], df_ex[df_ex['is_train']==False]
+train_be, test_be = df_be[df_be['is_train']==True], df_be[df_be['is_train']==False]
+
+# Concatenate the test and training data into two seperate dataframes
+train = pd.concat([train_ex, train_be])
+test = pd.concat([test_ex, test_be])
+
+
+
 # # Show the number of observations for the test and training dataframes
-# print('Number of observations in the training data:', len(train))
-# print('Number of observations in the test data:',len(test))
+print('Number of observations in the training data:', len(train))
+print('Number of observations in the test data:',len(test))
 
 # Initialize proxy data directories
 proxy_exploit_dir = "data/proxyData/exploitData"
