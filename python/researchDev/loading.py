88a64518da7c3b044e4bb83e2a8181c956e70a31
Michael Vaouli <mykelxknight@Michaels-Mini.home>
2017-12-04 22:50:33 -0800

    Update

--- python/researchDev/loading.py
+++ python/researchDev/loading.py
@@ -6,12 +6,12 @@ import sys
 done = False
 #loading animation
 def load_analyzer():
-    for f in itertools.cycle(['.','.','..','..','...','...','....','....','.....','.....']):
+    for f in itertools.cycle(['ᗧ','ᗧ*','*ᗧ*','**ᗧ*','***ᗧ*','****ᗧ*','*****ᗧ*','****ᗧ']):
         if done:
             break
         sys.stdout.write('\rAnalyzing File ' + f)
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(0.3)
     sys.stdout.write('\rDone!     ')
 
 t = threading.Thread(target=load_analyzer)
