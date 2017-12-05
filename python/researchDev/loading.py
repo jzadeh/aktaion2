87acae0cc55314c4ca9b6d7ceb340d27c3399d1d
Michael Vaouli <mykelxknight@Michaels-Mini.home>
2017-12-04 23:30:28 -0800

    Update

--- python/researchDev/loading.py
+++ python/researchDev/loading.py
@@ -6,13 +6,17 @@ import sys
 done = False
 #loading animation
 def load_analyzer():
-    for f in itertools.cycle(['ᗧ','ᗧ*','*ᗧ*','**ᗧ*','***ᗧ*','****ᗧ*','*****ᗧ*','****ᗧ']):
+    for f in itertools.cycle(['ᗧ*Analyzing*File*','*Aᗧnalyzing*File*','*Anᗧalyzing*File*','*Anaᗧlyzing*File*','*Analᗧyzing*File*',
+                              '*Analyᗧzing*File*','*Analyzᗧing*File*','*Analyziᗧng*File*','*Analyzinᗧg*File*','*AnalyzingᗧFile*',
+                              '*Analyzing*Fᗧile*','*Analyzing*Fiᗧle*','*Analyzing*Filᗧe*','*Analyzing Fileᗧ*']):
+    #for f in itertools.cycle(['ᗧ','ᗧ*','*ᗧ*','**ᗧ*','***ᗧ*','****ᗧ*','*****ᗧ*','****ᗧ']):
         if done:
             break
-        sys.stdout.write('\rAnalyzing File ' + f)
+        sys.stdout.write('\r' + f)
+        #sys.stdout.write('\rAnalyzing File ' + f)
         sys.stdout.flush()
         time.sleep(0.3)
-    sys.stdout.write('\rDone!     ')
+    sys.stdout.write('\rCompleted!')
 
 t = threading.Thread(target=load_analyzer)
 t.start()
